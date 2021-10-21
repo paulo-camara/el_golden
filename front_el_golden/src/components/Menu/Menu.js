86c7b4052b2f3fbb357c00d3aa4e1d6f6ef9bfea
Paulo Camara <paulo.camara@qintess.com>
2021-10-21 17:44:48 -0300

    Alterando ordem do menu

--- front_el_golden/src/components/Menu/Menu.js
+++ front_el_golden/src/components/Menu/Menu.js
@@ -5,15 +5,15 @@ export const Menu = () => {
     return (
         <div className="menu">
             <ItemMenu
-                title={"Gerenciamento de Dispositivos"}
-                redirectTo={'/management-device'}
-                icon={"https://cdn.icon-icons.com/icons2/1875/PNG/512/devices_119983.png"}
-            />
-            <ItemMenu
                 title={"Gerenciamento de Categorias"}
                 redirectTo={'/management-category'}
                 style={{ margin: '10px 10px', height: '80px', width: '80px' }}
                 icon={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe6wtM1ux-GVwq4jgk1dmQs-eAJxDY1sOEIV3aOoAT3cpyYhW3d1_fGj-Q_eNVNQevwK0&usqp=CAU"}
+            />
+             <ItemMenu
+                title={"Gerenciamento de Dispositivos"}
+                redirectTo={'/management-device'}
+                icon={"https://cdn.icon-icons.com/icons2/1875/PNG/512/devices_119983.png"}
             />
         </div>
     )
