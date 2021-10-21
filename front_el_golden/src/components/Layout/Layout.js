90d0b5926eaea2b617ad74ec101c541356e453a9
Paulo Camara <paulo.camara@qintess.com>
2021-10-21 17:48:02 -0300

    Alterando titulos

--- front_el_golden/src/components/Layout/Layout.js
+++ front_el_golden/src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Layout = ({ children }) => {
     return <div className="layout">
         <div className="container-layout">
-            <span className="title-principal">Gerenciamento El-Golden</span>
+            <span className="title-principal">Gerenciamento Eldorado</span>
         </div>
         {children}
     </div>
